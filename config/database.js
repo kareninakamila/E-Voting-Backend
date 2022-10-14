d6648f979f13c7f38d8f8fe7a8b5a550c6e55559
karen <kareninakamila@gmail.com>
2022-10-14 13:37:38 +0700

    update db

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'evoting.postgres.database.azure.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'voting'),
+      database: env('DATABASE_NAME', 'evoting'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      password: env('DATABASE_PASSWORD', 'Mysql12!'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });

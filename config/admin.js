5676da37289f97d8859400a902fb03d68f83bf71
karen <kareninakamila@gmail.com>
2022-10-15 19:51:57 +0700

    5

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'RopPPIKmJlMNo/RZWMimQw=='
   },
   apiToken: {
     salt: 'AtUFfl/nKmK/hm22g+VsZmg==',
